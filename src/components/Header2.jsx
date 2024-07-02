3f39fd5eabb613c3dc5761904ac486b5ba5415b1
ANKITKUMAR1809 <skwedsak143@gmail.com>
2024-07-02 16:40:18 +0530

    responsive committed

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -9,13 +9,13 @@ export const Header2 = () => {
     }, [cartCount, cart])
     return (
         <div className=''>
-            <div className='w-full flex flex-wrap flex-row justify-between items-center shadow-md py-4'>
+            <div className='w-full flex flex-wrap flex-row justify-between items-center shadow-md py-4 px-2'>
                 <h1 className='max-[600px]:w-full text-center text-3xl font-bold text-blue-950'>AS MALL</h1>
                 <div className="max-[600px]:w-full flex items-center justify-around searchbar border-2 border-blue-950  rounded-md">
                     <input type="search" className=' px-4 py-1 border-none outline-none rounded-md' />
                     <h3 className='cursor-pointer  text-blue-950 font-semibold'>Search</h3>
                 </div>
-                <ul className='max-[600px]:w-full  flex justify-evenly w-[20%]'>
+                <ul className='max-[600px]:w-full  flex justify-evenly gap-2'>
                     <NavLink to="" ><li className='text-blue-950 font-semibold text-[20px]'>Home</li> </NavLink>
                     <NavLink to="/men" ><li className='text-blue-950 font-semibold text-[20px]'>Men</li> </NavLink>
                     <NavLink to="/women" ><li className='text-blue-950 font-semibold text-[20px]'>Women</li></NavLink>
